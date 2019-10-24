641fbce21101d04bb5f66ca5a881f0bb66dc4784
Dononono <54520381+Dononono@users.noreply.github.com>
2019-10-24 14:17:55 -0700

    Add files via upload

---
+++ Moisture-Sensor-Lab.js
@@ -0,0 +1,18 @@
+let moistureLevel: number = 0
+
+basic.forever(function () {
+    // power up digital pin (sensor power)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    // read the analog pin (sensor output)
+    moistureLevel = pins.analogReadPin(AnalogPin.P0)
+    // power down digital pin
+    pins.digitalReadPin(DigitalPin.P16, 0)
+    // setting the scale that the microbit outputs based off the moistureLevel
+
+    // let scaledLevel =  map(moistureLevel , 10, 500, 0, 4) 
+    basic.showNumber(moistureLevel)
+    basic.pause(100)
+    basic.clearScreen()
+    basic.pause(1000)
+
+})
